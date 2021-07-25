a681351848e444678c7b0bebe258142f11820272
lsaac <2423860442@qq.com>
2021-07-25 12:07:18 +0800

    Initial Commit

---
+++ CommonPractise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CommonPractise
+//
+//  Created by lsaac on 2021/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
